8c07674d40a50d70cb423bb6935fa98b13c0aa15
Frank Giordano <giofr01@yahoo.com>
2020-01-25 16:51:03 -0500

    remove target directory

--- target/plattrk/WEB-INF/resources/js/custom/app.js
+++
@@ -1,1 +0,0 @@
-var app = angular.module('app', ['ngRoute', 'xeditable', 'LocalStorageModule', 'ngTable', 'smart-table', 'ui.bootstrap', 'ui.bootstrap.datetimepicker', 'ngGrid', 'angularModalService','multi-select', 'ngSanitize', 'ngCsv']);
