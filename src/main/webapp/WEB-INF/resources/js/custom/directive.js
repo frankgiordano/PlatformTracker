f4263974bfb2e9a53d11bcded5a10d9ea03e3a62
Frank Giordano <giofr01@yahoo.com>
2020-05-26 14:47:31 -0400

    #80 - Consistent handling of number fields in UI

--- src/main/webapp/WEB-INF/resources/js/custom/directive.js
+++ src/main/webapp/WEB-INF/resources/js/custom/directive.js
@@ -22,6 +22,32 @@ app.directive('myDatePickerStart', function () {
     };
 })
 
+app.directive('onlyDigits', function () {
+    return {
+        require: 'ngModel',
+        restrict: 'A',
+        link: function (scope, element, attr, ctrl) {
+            function inputValue(val) {
+                if (val) {
+                    var digits = val.replace(/[^0-9.]/g, '');
+
+                    if (digits.split('.').length > 2) {
+                        digits = digits.substring(0, digits.length - 1);
+                    }
+
+                    if (digits !== val) {
+                        ctrl.$setViewValue(digits);
+                        ctrl.$render();
+                    }
+                    return parseFloat(digits);
+                }
+                return undefined;
+            }
+            ctrl.$parsers.push(inputValue);
+        }
+    };
+});
+
 app.directive('myDatePickerEnd', function () {
     return {
         restrict: 'E',
