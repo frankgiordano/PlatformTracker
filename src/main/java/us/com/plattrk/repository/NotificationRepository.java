eb7e621faf883149cc36b96a89267e119d21f49e
Frank Giordano <giofr01@yahoo.com>
2020-10-17 21:10:43 -0400

    #163 - type tweak

--- src/main/java/us/com/plattrk/repository/NotificationRepository.java
+++ src/main/java/us/com/plattrk/repository/NotificationRepository.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import us.com.plattrk.api.model.Notification;
-import us.com.plattrk.api.model.Type;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
